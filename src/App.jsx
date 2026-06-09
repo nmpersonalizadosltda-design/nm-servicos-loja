e9015db7c4b7bbbe8e2996f99120aa7a5a7dccfe
Natália Teixeira Moreira <nataliatmoreira@gmail.com>
2026-06-09 19:36:05 -0300

    Adiciona rota do admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Loja from "./pages/Loja";
+import Admin from "./pages/Admin";
 
 function App() {
+  const caminho = window.location.pathname;
+
+  if (caminho === "/admin") {
+    return <Admin />;
+  }
+
   return <Loja />;
 }
 
